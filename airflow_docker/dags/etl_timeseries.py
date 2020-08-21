0fd4a3f1ae298ddba02ba188553da9c13435d20c
Veronica Castro <70034196+Hakaniemi@users.noreply.github.com>
2020-08-21 18:04:28 -0500

    Docker Files
    Docker Files

---
+++ airflow_docker/dags/etl_timeseries.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+ 
+import pandas as pd
+import time
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+
+ ## Time series ELT Dag that will retrieve data from designed URL in json format and save it locally as cvs file
+timestr = time.strftime("%Y%m%d-%H_%M")
+
+dag = DAG(
+   dag_id="etl_timeseries",
+   start_date=datetime(2020, 7, 1),
+   schedule_interval='0 * * * *', 
+)
+ 
+#fetch time series json data from URL 
+extract= BashOperator(
+   task_id="fetch_timeseries",
+   bash_command='curl -o /opt/airflow/files/timeseries_{timestamp}.json https://pomber.github.io/covid19/timeseries.json'.format(timestamp=timestr),
+   dag=dag,
+)
+ 
+ ## parse json data and writes it to output file
+def parse_json(input_path, output_path):
+   print("read_json ...")
+   timeseries = pd.read_json(input_path)
+   csv = timeseries
+   csv.to_csv(output_path, index=False)
+ 
+## task definition 
+## 
+transform_load = PythonOperator(
+   task_id="read_json",
+   python_callable=parse_json,
+   op_kwargs={
+       "input_path": "/opt/airflow/files/timeseries_{timestamp}.json".format(timestamp=timestr),
+       "output_path": "/opt/airflow/files/timeseries_{timestamp}.csv".format(timestamp=timestr),
+   },
+   dag=dag,
+)
+ 
+extract >> transform_load
